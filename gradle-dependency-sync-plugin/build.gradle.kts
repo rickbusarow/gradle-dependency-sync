57687854251975c1c803cb92662d8d7296447340
RBusarow <RBusarow@users.noreply.github.com>
2023-06-27 14:57:52 +0000

    Apply KtLint format
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- gradle-dependency-sync-plugin/build.gradle.kts
+++ gradle-dependency-sync-plugin/build.gradle.kts
@@ -73,7 +73,7 @@ tasks.withType<Test>().configureEach {
 kotlin {
   jvmToolchain(libs.versions.jvmTarget.get().toInt())
 }
-tasks.withType(JavaCompile::class.java) {
+tasks.withType(JavaCompile::class.java).configureEach {
   options.release.set(libs.versions.jvmTarget.get().toInt())
   val targetString = JavaVersion.values()
     .single { it.majorVersion == libs.versions.jvmTarget.get() }
